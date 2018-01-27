6bb0ac1544ea645a6e62f24b4d4791dfc4ce8bc9
DESKTOP-19689J4\Nihal <nihals25@gmail.com>
2018-01-27 16:32:03 -0600

    Authentication Changes 4

--- routes/authentication.js
+++ routes/authentication.js
@@ -26,8 +26,7 @@ router.post('/userexists', function(req, res) {
     });    
 });
 
-router.post('/register', function(req, res) {
-  console.log(req);
+router.post('/register', function(req, res) {  
     Account.register(new Account({ username : req.body.username, email : req.body.email }), req.body.password, function(err, account) {        
         if (err) {
             return res.render('register', { account : account });
