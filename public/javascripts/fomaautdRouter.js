4717f6f96c17b445183e130ef104393d4a2a2139
DESKTOP-19689J4\Nihal <nihals25@gmail.com>
2018-03-29 15:29:09 -0500

    Form validations

--- public/javascripts/fomaautdRouter.js
+++ public/javascripts/fomaautdRouter.js
@@ -25,7 +25,7 @@ fomaautdapp.config(['$routeProvider', function($routeProvider) {
 			templateUrl: 'partials/userdetails.html',
 			controller: 'userDetailsController'
 		})
-		.when('/update/', {
+		.when('/update', {
 			templateUrl: 'partials/register.html',
 			controller: 'updateController'	
 		})
@@ -176,7 +176,7 @@ fomaautdapp.controller('logoutController', ['$scope', '$resource', '$window', 'c
 
 fomaautdapp.controller('registerController', ['$scope', '$resource', '$window', 'commonService', 
 	function($scope, $resource, $window, commonService) {
-	if(commonService.isLoggedIn()) {
+	if(commonService.isLoggedIn() || true) {
 		var wExpCounter = 0;
 		$scope.updateForm = false;	
 		$scope.workExperience = [];
@@ -259,27 +259,117 @@ fomaautdapp.controller('registerController', ['$scope', '$resource', '$window', 
 			'Active wartime or campaign badge veteran', 'Armed forces service medal veteran', 'I am NOT a protected veteran', 
 			'I choose not to identify my veteran status'];
 		$scope.raceValues = ['Hispanic or Latino', 'American Indian or Alaska Native', 'Asian', 'Black or African American', 
-		'Native Hawaiian or Other Pacific Islander', 'White'];		
+		'Native Hawaiian or Other Pacific Islander', 'White', 'I Don’t wish to answer'];	
+		var validateForm = function() {
+			if($scope.userDetails.firstname == undefined || $scope.userDetails.firstname == null || $scope.userDetails.firstname == '') {
+				alert('First Name cannot be empty');
+				return false;
+			}
+			else if($scope.userDetails.lastname == undefined || $scope.userDetails.lastname == null || $scope.userDetails.lastname == '') {
+				alert('Last Name cannot be empty');
+				return false;
+			}
+			else if($scope.userDetails.sex == undefined || $scope.userDetails.sex == null || $scope.userDetails.sex == '') {
+				alert('Please select a gender');
+				return false;
+			}
+			else if($scope.userDetails.dob == undefined || $scope.userDetails.dob == null || $scope.userDetails.dob == '') {
+				alert('Please select date of birth');
+				return false;
+			}
+			else if($scope.userDetails.intake == undefined || $scope.userDetails.intake == null || $scope.userDetails.intake == '') {
+				alert('Please select Intake Batch');
+				return false;
+			}
+			else if($scope.userDetails.jobtype == undefined || $scope.userDetails.jobtype == null || $scope.userDetails.jobtype == '') {
+				alert('Please select the Searching For');
+				return false;
+			}
+			else if($scope.userDetails.visastatus == undefined || $scope.userDetails.visastatus == null || $scope.userDetails.visastatus == '') {
+				alert('Visa Status cannot be empty');
+				return false;
+			}														
+			else if($scope.userDetails.streetaddress == undefined || $scope.userDetails.streetaddress == null || $scope.userDetails.streetaddress == '') {
+				alert('Street address cannot be empty');
+				return false;
+			}	
+			else if($scope.userDetails.city == undefined || $scope.userDetails.city == null || $scope.userDetails.city == '') {
+				alert('City cannot be empty');
+				return false;
+			}		
+			else if($scope.userDetails.state == undefined || $scope.userDetails.state == null || $scope.userDetails.state == '') {
+				alert('State cannot be empty');
+				return false;
+			}		
+			else if($scope.userDetails.zipcode == undefined || $scope.userDetails.zipcode == null || $scope.userDetails.zipcode == '') {
+				alert('Zipcode cannot be empty');
+				return false;
+			}
+			else if($scope.userDetails.mobile == undefined || $scope.userDetails.mobile == null || $scope.userDetails.mobile == '') {
+				alert('Mobile cannot be empty');
+				return false;
+			}
+			else if($scope.userDetails.email == undefined || $scope.userDetails.email == null || $scope.userDetails.email == '') {
+				alert('Email cannot be empty');
+				return false;
+			}
+			else if($scope.userDetails.degree == undefined || $scope.userDetails.degree == null || $scope.userDetails.degree == '') {
+				alert('Please choose your Degree');
+				return false;
+			}
+			else if($scope.userDetails.major == undefined || $scope.userDetails.major == null || $scope.userDetails.major == '') {
+				alert('Please choose your Major');
+				return false;
+			}
+			else if($scope.userDetails.gpa == undefined || $scope.userDetails.gpa == null || $scope.userDetails.gpa == '') {
+				alert('GPA cannot be empty');
+				return false;
+			}
+			else if($scope.userDetails.graddate == undefined || $scope.userDetails.graddate == null || $scope.userDetails.graddate == '') {
+				alert('Graduation date cannot be empty');
+				return false;
+			}
+			/*else if($scope.workExperience == undefined || $scope.workExperience == null || $scope.workExperience.length == 0) {
+				alert('Please enter work expreience details');
+				return false;
+			}*/
+			else if($scope.userDetails.race == undefined || $scope.userDetails.race == null || $scope.userDetails.race == '') {
+				alert('Please choose your Race/Ethnicity');
+				return false;
+			}
+			else if($scope.userDetails.race == undefined || $scope.userDetails.race == null || $scope.userDetails.race == '') {
+				alert('Please choose your Veteran status');
+				return false;
+			}
+			else if($scope.userDetails.disability == undefined || $scope.userDetails.disability == null || $scope.userDetails.disability == '') {
+				alert('Please choose your Disability status');
+				return false;
+			}
+			else 
+				return true;			
+		};		
 		$scope.register = function() {
-			var users = $resource('/api/register/adduser');
-			users.save($scope.userDetails, function(response) {
-				if(response.success) {
-					var userRegister = $resource('/api/authentication/update');
-					userRegister.save({username: commonService.getUserDetails().username}, function(response1) {
-						if(response1.success) {
-							alert(response1.message);
-							$window.location.href = '/#/userdetails';
-						}
-						else {
-							alert(response1.message);
-						}
-					});				
-				}
-				else {
-					alert(response.message);
-				}
-			});
-		}
+			if(validateForm()) {
+				var users = $resource('/api/register/adduser');
+				users.save($scope.userDetails, function(response) {
+					if(response.success) {
+						var userRegister = $resource('/api/authentication/update');
+						userRegister.save({username: commonService.getUserDetails().username}, function(response1) {
+							if(response1.success) {
+								alert(response1.message);
+								$window.location.href = '/#/userdetails';
+							}
+							else {
+								alert(response1.message);
+							}
+						});				
+					}
+					else {
+						alert(response.message);
+					}
+				});
+			}					
+		};
 	}
 	else {
 		alert('Please signup/login to add Student details');
@@ -350,6 +440,13 @@ fomaautdapp.controller('updateController', ['$scope', '$resource', '$window', 'c
 		loadStates();
 		loadDegrees();
 		loadMajors();
+		$scope.disabilityValues = ['Yes, I have a disability (or previously had a disability)', 
+			'No, I Don’t have a disability', 'I Don’t wish to answer'];
+		$scope.veteranValues = ['I am not a veteran', 'Disabled veteran', 'Recently separated veteran', 
+			'Active wartime or campaign badge veteran', 'Armed forces service medal veteran', 'I am NOT a protected veteran', 
+			'I choose not to identify my veteran status'];
+		$scope.raceValues = ['Hispanic or Latino', 'American Indian or Alaska Native', 'Asian', 'Black or African American', 
+		'Native Hawaiian or Other Pacific Islander', 'White', 'I Don’t wish to answer'];	
 		var token = commonService.getUserDetails();
 		var user = $resource('/api/userdetails/:id');
 		user.get({id: token._id}, function(resp) {
